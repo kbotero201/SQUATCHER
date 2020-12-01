cebf512752e037985af48d156d5fb096e297dde1
kbotero201 <kbotero201@gmail.com>
2020-12-01 17:01:17 -0500

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
   resources :expeditions
   resources :users
   resources :cryptids
+  resources :sessions
 
    #get 'login', to: 'sessions#new'
    #post 'login', to: 'sessions#create'
    #get 'welcome', to: 'sessions#welcome'
 
-   delete '/sessions/logout', to: 'sessions/logout', as: 'logout'
-   get "/sessions/new", to: 'sessions#new', as: 'new_login'
-   post '/sessions/login', to: 'sessions#login', as: 'login'
+   #delete '/sessions/logout', to: 'sessions/logout', as: 'logout'
+   #get "/sessions/new", to: 'sessions#new', as: 'new_login'
+   #post '/sessions/login', to: 'sessions#login', as: 'login'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
