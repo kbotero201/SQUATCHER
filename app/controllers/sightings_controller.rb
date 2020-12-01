ba1eeb41389ffabdea4e1b309f7bd49c4b8724fd
Amodttech <aaron@amodttech.com>
2020-11-30 20:38:17 -0500

    controller actions built

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,2 +1,57 @@
 class SightingsController < ApplicationController
+
+    def index
+        @sightings = Sighting.all
+    end
+
+    def show
+        find_sighting
+    end
+
+    def new
+        @sighting = Sighting.new
+    end
+
+    def create
+        sighting = Sighting.create(sighting_params)
+        if sighting.valid?
+            redirect_to sighting_path(sighting)
+        else
+            flash[:sighting_errors] = sighting.errors.full_messages
+            redirect_to new_sighting_path
+        end
+    end
+
+    def edit
+        find_sighting
+    end
+
+    def update
+        find_sighting
+        if @sighting.valid?
+            @sighting.update(sighting_params)
+            redirect_to sighting_path(@sighting)
+        else
+            flash[:sighting_errors] = @sighting.errors.full_messages
+            redirect_to new_sighting_path
+        end
+    end
+
+    def destroy
+        find_sighting
+        @sighting.destroy
+        redirect_to sightings_path
+    end
+
+    private
+
+    def find_sighting
+        @sighting = Sighting.find(params[:id])
+    end
+
+    def sighting_params
+        params.require(:sighting).permit(:date, :location, :creature_temperament, :fear_rating, :discription, :expedition_id, :user_id, :cryptid_id)
+    end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,57 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+    end
+
+    def show
+        find_user
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            redirect_to user_path(user)
+        else
+            flash[:user_errors] = user.errors.full_messages
+            redirect_to new_user_path
+        end
+    end
+
+    def edit
+        find_user
+    end
+
+    def update
+        find_user
+        if @user.valid?
+            @user.update(user_params)
+            redirect_to user_path(@user)
+        else
+            flash[:user_errors] = @user.errors.full_messages
+            redirect_to new_user_path
+        end
+    end
+
+    def destroy
+        find_user
+        @user.destroy
+        redirect_to users_path
+    end
+
+    private
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :years_experience)
+    end
+
+
 end
