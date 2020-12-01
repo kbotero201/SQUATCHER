0f248736e41ea29a121a2412b654275e263f4397
kbotero201 <kbotero201@gmail.com>
2020-12-01 16:17:44 -0500

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-    before_action: current_user
+    before_action :current_user
 
 #helper_method :current_user
 #helper_method :logged_in?
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
    #post 'login', to: 'sessions#create'
    #get 'welcome', to: 'sessions#welcome'
 
-   delete '/sessions/logout', to: 'sessions/logout', as: 'logout'
-   get "/sessions/new", to: 'sessions#new', as: 'new_login'
-   post '/sessions/login', to: 'sessions#login', as: 'login'
+  #  delete '/sessions/logout', to: 'sessions/logout', as: 'logout'
+  #  get "/sessions/new", to: 'sessions#new', as: 'new_login'
+  #  post '/sessions/login', to: 'sessions#login', as: 'login'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
