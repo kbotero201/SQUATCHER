1d7d4b38900483910620f88fe9317fe5a04b5724
kbotero201 <kbotero201@gmail.com>
2020-12-01 18:06:15 -0500

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,11 @@ class ApplicationController < ActionController::Base
 
     before_action :current_user
 
-#helper_method :current_user
-#helper_method :logged_in?
-
 
     def current_user
         #User.find_by(id: session[:user_id])
         @current_user = User.find_by(id: session[:user_id])
     end
 
-    #def logged_in?     
-    #    !current_user.nil?
-    #end
 
 end
