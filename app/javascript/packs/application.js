c66ba805a5459d514ffb21d030a835569ffd48cd
kbotero201 <kbotero201@gmail.com>
2020-12-02 16:55:23 -0500

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,16 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+import "pickadate/default";
+import "pickadate/default.date"; 
+
+//= require pickadate/picker
+//= require pickadate/picker.date
+
+$('.datepicker').pickadate()
+
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
