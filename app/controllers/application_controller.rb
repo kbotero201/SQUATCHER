ea8e226173728be44aded5f6701137e98cbd22d7
Amodttech <aaron@amodttech.com>
2020-12-02 12:22:52 -0500

    application controller authorization method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
 
     before_action :current_user
+    before_action :authorized
 
 
     def current_user
         #User.find_by(id: session[:user_id])
         @current_user = User.find_by(id: session[:user_id])
     end
+
+    def authorized
+        redirect_to new_user_path unless @current_user
+      end 
 
 
 end
