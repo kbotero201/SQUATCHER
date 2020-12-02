dcd7ab28de86f7c07be1dd8c536a1651df492cec
kbotero201 <kbotero201@gmail.com>
2020-12-02 17:46:11 -0500

    kevin changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
    get "/sessions/new", to: 'sessions#new', as: 'new_login'
    post '/sessions/login', to: 'sessions#login', as: 'login'
 
-   get 'sessions/logout', to: 'sessions#logout', as: 'now_logout'
-  delete '/sessions/logout', to: 'sessions#logout', as: 'logout'
+   #get 'sessions/logout', to: 'sessions#logout', as: 'now_logout'
+   #delete '/sessions/logout', to: 'sessions#logout', as: 'logout'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
