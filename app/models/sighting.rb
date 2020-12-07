6274d3c9a8cb45c1a6b554ea024bb9860dd41166
kbotero201 <kbotero201@gmail.com>
2020-12-07 13:56:19 -0500

    new

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -3,7 +3,7 @@ class Sighting < ApplicationRecord
     belongs_to :expedition
     belongs_to :user
 
-    validates_format_of :date, :with => /\d{4}-\d{2}-\d{2}/, :message => "Date must be in the following format: yyyy/mm/dd"
+    validates_format_of :date, :with => /\d{4}-\d{2}-\d{2}/, :message => "must be in the following format: yyyy/mm/dd"
     validates :date, presence: true 
     validates :location, presence: true 
     #validates :description, presence: true 
